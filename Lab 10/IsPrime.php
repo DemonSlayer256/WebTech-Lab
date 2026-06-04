bb5e53c955740d24703b0f8fedfbed19bc0e6736
DemonSlayer256 <143187253+DemonSlayer256@users.noreply.github.com>
2026-06-04 07:38:29 +0000

    Refactored IsPrime.php to function as intended

--- Lab 10/IsPrime.php
+++ Lab 10/IsPrime.php
@@ -1,27 +1,53 @@
 <?php
-if (isset($_POST['number'])) {
-    $num = intval($_POST['number']);
-    $isPrime = true;
+// 1. Initialize variables to avoid "undefined variable" notices
+$num = "";
+$isPrime = true;
+$searched = false;
 
-    if ($num < 2) {
+// 2. Check if the form was actually submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['number'])) {
+    $num = intval($_POST['number']);
+    $searched = true;
+
+    // 0 and 1 are not prime numbers
+    if ($num <= 1) {
         $isPrime = false;
     } else {
+        // 3. Prime number logic: check for factors up to the square root of the number
         for ($i = 2; $i <= sqrt($num); $i++) {
             if ($num % $i == 0) {
                 $isPrime = false;
-                break;
+                break; // Exit loop early if a factor is found
             }
         }
     }
-
-    if ($isPrime) {
-        echo "$num is a prime number.";
-    } else {
-        echo "$num is not a prime number.";
-    }
 }
 ?>
-<form method="post">
-    Enter a number: <input type="number" name="number" required>
-    <input type="submit" value="Check">
-</form>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prime Number Checker</title>
+</head>
+<body>
+
+    <form method='post'>
+        Enter a number: <input type='number' name='number' value="<?php echo htmlspecialchars($num); ?>" required>
+        <input type='submit' value='Check'>
+    </form>
+
+    <br>
+
+    <?php
+    // 4. Only display the result if the user actually clicked submit
+    if ($searched) {
+        if ($isPrime) {
+            echo "<strong>$num is a prime number.</strong>";
+        } else {
+            echo "<strong>$num is not a prime number.</strong>";
+        }
+    }
+    ?>
+
+</body>
+</html>
